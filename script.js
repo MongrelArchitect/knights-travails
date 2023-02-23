3034220d15b02333338e05faa18e0b2c6b73dfb2
Sean Eric Thomas <set@seanericthomas.com>
2023-02-23 12:19:23 -0800

    Find a shortest path between start and end coordinates

--- script.js
+++ script.js
@@ -71,8 +71,101 @@ function buildGraph() {
 
 const Graph = () => {
   const graph = buildGraph();
-  return graph;
+
+  const getKey = (coord) => {
+    // Our graph vertices store their neighbors as integer keys, so we
+    // need to convert coordinates to these keys when search starts
+    const keys = Object.keys(graph);
+    let result;
+    keys.every((key) => {
+      const vertex = graph[key];
+      if (vertex.coord[0] === coord[0] && vertex.coord[1] === coord[1]) {
+        result = key;
+        return false;
+      }
+      return true;
+    });
+    return result;
+  };
+
+  const displayPath = (path) => {
+    // For a somewhat cohert display of our final path
+    const finalPath = [];
+    path.forEach((key) => {
+      finalPath.push(graph[key].coord);
+    });
+    console.log(`Start: [${finalPath[0]}]`);
+    console.log(`  End: [${finalPath[finalPath.length - 1]}]`);
+    console.log('');
+    console.log(`MADE IT IN ${finalPath.length - 1} MOVES`);
+    console.log('==================');
+    finalPath.forEach((coord) => {
+      console.log(` [${coord}]`);
+    });
+  };
+
+  const getPath = (visited, origins) => {
+    // Start at our destination and backtrack where each visited vertex
+    // came from to find our shortest path
+    const end = visited[[visited.length - 1]];
+    const reversePath = [end];
+    let previous = origins[[origins.length - 1]];
+    while (previous) {
+      reversePath.push(previous);
+      const visitedIndex = visited.indexOf(previous);
+      previous = origins[visitedIndex];
+    }
+    // Flip our path around to the correct order
+    return reversePath.reverse();
+  };
+
+  const knightMoves = (start, end) => {
+    const destination = getKey(end);
+    const queue = [getKey(start)];
+
+    // Need to keep track of visited vertices and their origins
+    const visited = [];
+    const origins = [];
+    let shortestPath;
+
+    // First one to search has no origin vertex
+    graph[queue[0]].cameFrom = null;
+
+    while (queue.length > 0) {
+      const key = queue[0];
+      // Don't need to check this vertex again
+      if (!visited.includes(key)) {
+        visited.push(key);
+        // Keep track of where this vertex came from
+        origins.push(graph[key].cameFrom);
+      }
+
+      if (key === destination) {
+        // Found it! Time to wrap it up and backtrack to find our path
+        shortestPath = getPath(visited, origins);
+        break;
+      }
+
+      // Enqueue all current vertex neighbors for breadth first search
+      graph[key].neighbors.forEach((neighbor) => {
+        // Don't enqueue if already visited or currently in queue
+        if (!visited.includes(neighbor) && !queue.includes(neighbor)) {
+          // Enqueue & keep track of where it came from
+          queue.push(neighbor);
+          graph[neighbor].cameFrom = key;
+        }
+      });
+
+      // All done with this vertex
+      queue.shift();
+    }
+
+    // Display our path using coordinates instead of graph keys
+    displayPath(shortestPath);
+  };
+
+  return { graph, knightMoves };
 };
 
 const knight = Graph();
-console.log(knight);
+knight.knightMoves([1, 0], [7, 6]);
